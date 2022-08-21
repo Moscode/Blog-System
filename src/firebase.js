7b400e193d96d0e3975f61656d1fb4b972b4c669
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-21 10:59:24 +0000

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCUPWGPUkId4M8GXA-KR_8nESPrMOYudww",
+    authDomain: "blog-system-890b0.firebaseapp.com",
+    projectId: "blog-system-890b0",
+    storageBucket: "blog-system-890b0.appspot.com",
+    messagingSenderId: "14237902618",
+    appId: "1:14237902618:web:5b3485ec0db41cd3686ea3",
+    measurementId: "G-99GSDFY3QZ"
+});
+
+export default firebase;
